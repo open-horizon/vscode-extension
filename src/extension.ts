fec6906f0a8097df14bfd5526c1f1df803e5efb2
Will Lewis <whhlct22@Gmail.com>
2023-03-24 17:22:36 -0400

    First draft of SSH impl., removed test commands

--- src/extension.ts
+++ src/extension.ts
@@ -11,17 +11,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "openhorizon" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('openhorizon.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from OpenHorizon!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	context.subscriptions.push(
         vscode.commands.registerCommand('openHorizon.createService', async () => {
             await createService();
